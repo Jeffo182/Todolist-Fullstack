bf5dab3858f8b7a3f6e5dcb5ba9c183817d1d436
Jefferson Silva Felix <jefferson.felix182@gmail.com>
2023-02-16 09:48:47 -0300

    feat: cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const { route } = require('./routes/tasks');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(route);
 
 module.exports = { app };
